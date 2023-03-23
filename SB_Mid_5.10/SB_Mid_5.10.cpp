36a32ed269f7e30c2ec99f93c8ca5c2f7b9b32bf
Pavlovsky Nikolay <pavlovsky-n@ya.ru>
2023-03-23 16:10:15 +0600

    Добавьте файлы проекта.

---
+++ SB_Mid_5.10/SB_Mid_5.10.cpp
@@ -0,0 +1,45 @@
+﻿
+#include <iostream>
+#include <stdexcept>
+
+class Fraction
+{
+public:
+    float numerator;
+    float denominator;
+
+    Fraction(float num = 0, float den = 0) : numerator(num), denominator(den)
+    {
+        if (denominator == 0) 
+        {
+            throw std::runtime_error("denominator = 0");
+        }
+
+    }
+
+    ~Fraction(){}
+
+    float Div() {return numerator / denominator; }
+    
+};
+
+int main()
+{
+   try 
+   {
+       float num, den;
+       
+        std::cin.exceptions(std::istream::failbit);
+        std::cout << "Enter numerator: ";
+        std::cin >> num;
+        std::cout << "Enter denominator: ";
+        std::cin >> den;
+       Fraction v(num, den);
+       std::cout << v.numerator << "/" << v.denominator << " = " << v.Div() << "\n";
+    }
+   catch (std::exception& v) 
+   {
+      std::cout << "Error: " << v.what() << std::endl;
+   }
+}
+
